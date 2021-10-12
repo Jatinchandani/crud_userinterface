248e93ad2f2912901c003726b328196d0e817041
jatin <jatinchandani8@gmail.com>
2021-10-12 17:27:32 +0530

    final changes

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const app = express(); //Express method use
 require('dotenv').config(); //.Env module require
 const bodyParser = require('body-parser')
 const router=require('./src/router/index'); //Require route path
-const swaggerUi = require('swagger-ui-express'),
-    swaggerDocument = require('./swagger.json');
-
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -17,9 +14,6 @@ app.get('/',(req, res) => res.send('Notes App'));
 
 //Router 
 app.use('/api/v1', router);
-
-//Swagger router
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const db = require("./src/model");
 // db.sequelize.sync();
